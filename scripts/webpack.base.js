0fa3dc1499d6c33190277dc5d354d7af17656323
angmie <369822144@qq.com>
2024-07-31 18:19:49 +0800

    webpack init

---
+++ scripts/webpack.base.js
@@ -0,0 +1,109 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
+module.exports = {
+    entry: path.resolve(__dirname, '../src/index.jsx'),
+    output: {
+        path: path.resolve(__dirname, '../dist'), // 打包后的代码放在dist目录下
+        filename: '[name].[hash:8].js', // 打包的文件名
+        clean: true, // 打包前清理文件
+    },
+    resolve: {
+        // 配置 extensions 来告诉 webpack 在没有书写后缀时，以什么样的顺序去寻找文件
+        extensions: ['.mjs', '.js', '.json', '.jsx', '.ts', '.tsx'], // 如果项目中只有 tsx 或 ts 可以将其写在最前面
+        alias: {
+            '@src': path.resolve(__dirname, '../src')
+        }
+    },
+    optimization: {
+        minimize: true, // 开发环境也启用 CSS 优化
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false
+            }),
+            new CssMinimizerPlugin({}),
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../index.html'), // 使用自定义模板
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/css/[hash:8].css', // 将 css 单独提测出来放在assets/css 下
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /.(jsx?)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    targets: 'iOS 9, Android 4.4, last 2 versions, > 0.2%, not dead', // 根据项目去配置
+                                    useBuiltIns: 'usage', // 会根据配置的目标环境找出需要的polyfill进行部分引入
+                                    corejs: 3, // 使用 core-js@3 版本
+                                },
+                            ],
+                            ['@babel/preset-react'],
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    // 'style-loader',
+                    MiniCssExtractPlugin.loader, // 使用 MiniCssExtractPlugin.loader 代替 style-loader
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    ['postcss-preset-env', {}]
+                                ]
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+                // 排除 node_modules 目录
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|webp)$/i,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 25 * 1024, // 25kb
+                    },
+                },
+                generator: {
+                    filename: 'assets/imgs/[name].[hash:8][ext]',
+                },
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/i,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 25 * 1024, // 25kb
+                    },
+                },
+                generator: {
+                    filename: 'assets/fonts/[name].[hash:8][ext]',
+                },
+            },
+
+        ],
+    },
+
+}
